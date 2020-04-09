f8aea75e08a5b2c7dcd5b4e9ee5b6dc6851d098b
Doug Chestnut <dougchestnut@gmail.com>
2020-04-09 15:23:39 -0400

    added terser to rollup build

--- packages/Aeon-components/rollup.config.js
+++ packages/Aeon-components/rollup.config.js
@@ -1,6 +1,7 @@
 // rollup.config.js
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import { terser } from "rollup-plugin-terser";
 
 export default {
   input: 'lib/Aeon-components.js',
@@ -15,6 +16,7 @@ export default {
         moduleDirectory: 'node_modules'
       }
     }),
-    commonjs()
+    commonjs(),
+    terser()
   ]
 };
