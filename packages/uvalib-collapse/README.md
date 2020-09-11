fa3c025da2f6033bb915682847605232e7be47f4
Doug Chestnut <dougchestnut@gmail.com>
2020-09-11 10:01:45 -0400

    added start of uvalib-collapse, simple iron-collapse replacement

---
+++ packages/uvalib-collapse/README.md
@@ -0,0 +1,11 @@
+# `uvalib-collapse`
+
+> TODO: description
+
+## Usage
+
+```
+const uvalibCollapse = require('uvalib-collapse');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/uvalib-collapse/uvalib-collapse.js
@@ -0,0 +1,69 @@
+class UvalibCollapse extends HTMLElement {
+  static get observedAttributes() {
+    return ['opened'];
+  }
+  constructor() {
+    // Always call super first in constructor
+    super();
+    this.opened = false;
+  }
+  connectedCallback() {
+    this._eval(false);
+    this._setupDom();
+  }
+  _setupDom(){
+    // setup a shadowDOM
+    this.shadow = this.attachShadow({mode: 'open'});
+    this.shadow.innerHTML = `
+      <style>
+        :host {
+          display: block;
+          overflow: hidden;
+        }
+      </style>
+      <slot></slot>
+    `;
+    this.shadow.querySelector('slot').addEventListener('slotchange', function(e) {
+      this._eval(false);
+    }.bind(this))
+  }
+  attributeChangedCallback(name, oldValue, newValue) {
+    switch(name){
+      case "opened":
+        if (oldValue != newValue) {
+          this.opened = (newValue===null)? false:true;
+          this._eval().then(function(){
+            this.dispatchEvent(new CustomEvent('transitioning-changed', {detail: {opened: this.opened}}));
+          }.bind(this));
+          this.dispatchEvent(new CustomEvent('opened-changed', {detail: {opened: this.opened}}));
+        }
+        break;
+    }
+  }
+  _eval(ani=true){
+    var p = Promise.resolve();
+    if(ani) {
+      var h = this.scrollHeight;
+      p = this._animate(h,(!this.opened));
+    } else {
+      this.style.height = (this.opened)? "auto":"0px";
+    }
+    return p;
+  }
+  _animate(h,reverse=false){
+    this.transitioning = true;
+    var animate = this.animate((reverse)?{height:[h+'px',0]}:{height:[0,h+'px']},{easing:'ease-in-out', duration:h*1.5, fill:"forwards"});
+    return animate.finished.then(function(){
+        this.style.height = (reverse)?"0":"auto";
+        animate.cancel();
+        this.transitioning = false;
+    }.bind(this));
+  }
+  toggle(){
+    this.opened = !this.opened;
+    if (this.opened) this.setAttribute('opened',"");
+    else this.removeAttribute('opened');
+  }
+}
+
+window.customElements.define('uvalib-collapse', UvalibCollapse);
