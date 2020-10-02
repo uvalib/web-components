ba0527b71b0830f2f86dcfd9fef4cc74d66834fb
Doug Chestnut <dougchestnut@gmail.com>
2020-10-02 10:20:06 -0400

    working in some fixes

--- packages/uvalib-alerts/uvalib-alerts.js
+++ packages/uvalib-alerts/uvalib-alerts.js
@@ -9,23 +9,50 @@ uvalibAlertsStyles.replaceSync(style);
 document.adoptedStyleSheets = [...document.adoptedStyleSheets, uvalibAlertsStyles];
 
 class UvalibAlerts extends HTMLElement {
+  // listen to these attributes for override info
   static get observedAttributes() {
     return ['override','alerts'];
   }
+  // getters/setters properties for override info
+  get override() {return !!(this._override);}
+  set override(newOverride) {this._override = !!(newOverride); console.log("override set " +this.override)}
+  get alerts() {
+    return this._alerts;
+  }
+  set alerts(newAlerts) {
+    this._alerts = (Array.isArray(newAlerts))? newAlerts:[];
+    this._updateAlerts(this._alerts);
+  }
+
+  // attribute values
+  attributeChangedCallback(name, oldValue, newValue) {
+    switch(name){
+      case "override":
+        this.override = newValue===""? true:false;
+        break;
+      case "alerts":
+        this.alerts = JSON.parse(newValue);
+        break;
+    }
+  }
+
   constructor() {
     // Always call super first in constructor
     super();
-  }
-  connectedCallback() {
     this._setupDom();
-    if (!this.override) this._setupAlertsModel();
   }
+
+  // attributes should be parsed by the time connected callback is called
+  connectedCallback() {
+    if (!this.override) 
+      this._setupAlertsModel();
+  }
+
   _setupAlertsModel(){
     import('@uvalib/uvalib-models/uvalib-model-alerts.js').then(function(){
       this._alertsModel = document.createElement('uvalib-model-alerts');
       import ('lodash/debounce').then(function(debounce){
-        this._alertsModel.addEventListener('seen-count-changed',debounce.default(function(e){
-console.log("seen count changed");          
+        this._alertsModel.addEventListener('seen-count-changed',debounce.default(function(e){        
           const count = (e.detail && e.detail.seenCount)? parseInt(e.detail.seenCount):0;
           this.seenCount = count;
           this.setAttribute('seen-count', count);
@@ -33,7 +60,6 @@ console.log("seen count changed");
           this._updateAlerts(this._alertsModel.alerts);
         }.bind(this),300).bind(this));
         this._alertsModel.addEventListener('alerts-changed',debounce.default(function(e){
-console.log("alerts changed");
           this._alertsSeen = this._alertsModel.seen;
           this._updateAlerts(this._alertsModel.alerts);
         }.bind(this),300).bind(this));
@@ -42,60 +68,59 @@ console.log("alerts changed");
       this.shadow.appendChild(this._alertsModel);
     }.bind(this));
   }
+
   _updateAlerts(alerts){
     if (Array.isArray(alerts) && alerts.length>0) {
-      // be sure that we have style, uvalib-collapse, and uvalib-button
       this._setupStyle();
-      var newContainer = document.createElement('div');
-console.log(this._alertsSeen);      
-      alerts.filter(function(alert){console.log(alert); return !this._alertsSeen.includes(alert.uuid); }.bind(this))
-            .forEach(function(alert){ this._addAlert(newContainer, alert) }.bind(this));
+      var newContainer = document.createElement('div');          
+      const atemp = alerts.filter(function(alert){ return (this._alertsSeen)? !this._alertsSeen.includes(alert.uuid):true; }.bind(this))
+            .sort((a,b)=>(a.severity > b.severity)? 1:-1);
+      if (atemp.length > 0) {  
+        var importPromises = [];
+        importPromises.push(import ('@uvalib/uvalib-button'));
+        importPromises.push(import('@uvalib/uvalib-collapse'));
+        Promise.all(importPromises).then(function(imports) {       
+            atemp.forEach(function(alert){ this._addAlert(newContainer, alert) }.bind(this));
+        }.bind(this))
+      }
       this.shadow.replaceChild(newContainer, this._alertsContainer);
       this._alertsContainer = newContainer;
     } else {
       this._alertsContainer.innerHTML = "";
     }
   }
+
   _addAlert(newContainer, alert){
-    var importPromises = [];
-    importPromises.push(import ('lodash/debounce'));
-    importPromises.push(import ('@uvalib/uvalib-button'));
-    importPromises.push(import('@uvalib/uvalib-collapse'));
-    Promise.all(importPromises).then(function(imports){
-      var debounce = imports[0].default;
-      var node = document.createElement('uvalib-collapse');
-      node.innerHTML = `
-        <div class="alert-item ${alert.severity}" uuid="${alert.uuid}">
-          <div data-title="${alert.title}">
-            <div class="alert-head">
-              <div class="alert-title" >${alert.title}</div>
-              <uvalib-button mode="tertiary" class="moreButton toggle" ${this._isHot(alert.severity)? "hidden ":""}>More</uvalib-button>
-              <uvalib-button mode="tertiary" class="lessButton toggle" ${this._isHot(alert.severity)? "":"hidden"}>Less</uvalib-button>
-              <uvalib-button mode="tertiary" class="dismissButton" ${this._isHot(alert.severity)? "hidden ":""}>Dismiss</uvalib-button>
-            </div>
-            <uvalib-collapse class="bodyCollapse" ${this._isHot(alert.severity)?"opened":""} ${(this._isHot(alert.severity))?"opened":""}>
-              <div class="alert-body">${alert.body}</div>
-            </uvalib-collapse>
+    var node = document.createElement('uvalib-collapse');
+    node.innerHTML = `
+      <div class="alert-item ${alert.severity}" uuid="${alert.uuid}">
+        <div data-title="${alert.title}">
+          <div class="alert-head">
+            <div class="alert-title" >${alert.title}</div>
+            <uvalib-button mode="tertiary" class="moreButton toggle" ${this._isHot(alert.severity)? "hidden ":""}>More</uvalib-button>
+            <uvalib-button mode="tertiary" class="lessButton toggle" hidden>Less</uvalib-button>
+            <uvalib-button mode="tertiary" class="dismissButton" ${this._isHot(alert.severity)? "hidden ":""}>Dismiss</uvalib-button>
           </div>
+          <uvalib-collapse class="bodyCollapse" ${this._isHot(alert.severity)?"opened":""} ${(this._isHot(alert.severity))?"opened":""}>
+            <div class="alert-body">${alert.body}</div>
+          </uvalib-collapse>
         </div>
-      `;
-      this._moreButton = node.querySelector(".moreButton");
-      this._moreButton.addEventListener('click',this._toggleIt.bind(this));
-      this._lessButton = node.querySelector(".lessButton");
-      this._lessButton.addEventListener('click',this._toggleIt.bind(this));
-      this._dismissButton = node.querySelector('.dismissButton');
-      this._dismissButton.addEventListener('click',this._dismissIt.bind(this));
-      node.setAttribute('opened',(alert.seen)?null:"");
-      node.addEventListener('opened-changed',this._sizeChanged.bind(this));
-      node.addEventListener('transitioning-changed',this._sizeChanged.bind(this));
-      newContainer.appendChild(node);
-      this.style.display = "block";
-    }.bind(this));
+      </div>
+    `;
+    node.querySelector(".moreButton").addEventListener('click',this._toggleIt.bind(this));
+    node.querySelector(".lessButton").addEventListener('click',this._toggleIt.bind(this));
+    node.querySelector('.dismissButton').addEventListener('click',this._dismissIt.bind(this));
+    node.setAttribute('opened',(alert.seen)?null:"");
+    node.addEventListener('opened-changed',this._sizeChanged.bind(this));
+    node.addEventListener('transitioning-changed',this._sizeChanged.bind(this));
+    newContainer.appendChild(node);
+    this.style.display = "block";
   }
+
   _setupStyle(){    
     this.shadow.adoptedStyleSheets = [uvalibAlertsStyles];
 // todo: attempt to only load constructed styles if called (we have alerts) 
-// was causing an error in Safari
+// this was causing an error in Safari
 //    if (!document.uvalibSheetLoaded) {
 //      import('./uvalib-alerts.scss').then(function(style){
 //        sheet.replace(style.default).then(function(){
@@ -114,16 +139,6 @@ console.log(this._alertsSeen);
     // and a place to stash the alerts
     this._alertsContainer = document.createElement('div');
     this.shadow.appendChild(this._alertsContainer);
-  }
-  attributeChangedCallback(name, oldValue, newValue) {
-    switch(name){
-      case "override":
-        this.override = newValue===""? true:false;
-        break;
-      case "alerts":
-        this.alerts = JSON.parse(newValue);
-        break;
-    }
   }
   _sizeChanged(){
     this.dispatchEvent(new CustomEvent('size-changed', {detail: {height: this.clientHeight}}));
