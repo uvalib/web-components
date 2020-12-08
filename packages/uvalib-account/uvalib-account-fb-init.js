d892f78eaa69b04acc7257edb8ab6586e50880a7
Doug Chestnut <dougchestnut@gmail.com>
2020-12-08 05:48:51 -0500

    add support for multiple db

--- packages/uvalib-account/uvalib-account-fb-init.js
+++ packages/uvalib-account/uvalib-account-fb-init.js
@@ -6,3 +6,17 @@ export let app = firebase.default.initializeApp({
       projectId: "uvalib-api",
       appId: "1:602799472461:web:d56a8ec950c9c85edc8104"
     });
+
+export let occupancyapp = firebase.initializeApp({
+      apiKey: "AIzaSyDsTrjUL9kRug7fw_sNU31cy7JYzJAUvmQ",
+      databaseURL: "https://uvalib-api-occupancy.firebaseio.com",
+      projectId: "uvalib-api",
+      appId: "1:602799472461:web:d56a8ec950c9c85edc8104"
+}, "occupancy");
+  
+export let apiapp = firebase.initializeApp({
+      apiKey: "AIzaSyDsTrjUL9kRug7fw_sNU31cy7JYzJAUvmQ",
+      databaseURL: "https://uvalib-api.firebaseio.com/",
+      projectId: "uvalib-api",
+      appId: "1:602799472461:web:d56a8ec950c9c85edc8104"
+}, "api");
