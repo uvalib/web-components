6ad0a523ee177325aa9bec9709849477131f842b
Doug Chestnut <dougchestnut@gmail.com>
2021-02-08 19:14:15 -0500

    working on testing line counter

--- packages/uvalib-counts/uvalib-counter.js
+++ packages/uvalib-counts/uvalib-counter.js
@@ -6,6 +6,11 @@ import '@uvalib/uvalib-account/uvalib-account-auth.js';
 
 import('@vaadin/vaadin-text-field/vaadin-number-field.js');
 import'@vaadin/vaadin-select/vaadin-select.js';
+import('@vaadin/vaadin-button/vaadin-button.js');
+import('@polymer/iron-icon/iron-icon.js');
+import('@vaadin/vaadin-icons/vaadin-icons.js');
+import('@polymer/iron-localstorage/iron-localstorage.js');
+import('@polymer/paper-spinner/paper-spinner-lite.js');
 
 import '@uvalib/uvalib-account/uvalib-account-fb-init.js';
 import('@uvalib/uvalib-models/uvalib-model-realtime-database.js');
@@ -58,7 +63,9 @@ class UvalibCounter extends PolymerElement {
 <div id="container" class$="[[_isRecount(recount)]]">
       <uvalib-account-auth id="auth" auto userkey="[[_getUserKey()]]"></uvalib-account-auth>
    
-<vaadin-select value="{{selectedLocation}}" label="Location">
+<iron-localstorage name="my-selected-location" value="{{selectedLocation}}"
+on-iron-localstorage-load-empty="_initSelectedLocation"></iron-localstorage>     
+<vaadin-select title="Select the location that you are tracking" value="{{selectedLocation}}" label="Location">
   <template>
     <vaadin-list-box>
       <template is="dom-repeat" items="[[_filter(libraries,type)]]" as="location">
@@ -67,6 +74,10 @@ class UvalibCounter extends PolymerElement {
     </vaadin-list-box>
   </template>
 </vaadin-select>
+<vaadin-button title="Find the closest location" on-click="_locateMe" hidden$="[[!_locationAvailable()]]">
+  <iron-icon icon="vaadin:map-marker"></iron-icon>
+</vaadin-button>
+<paper-spinner-lite active="[[_geoworking]]" alt="Finding the location closest to you"></paper-spinner-lite>
 
 <template is="dom-repeat" items="[[_filter(libraries,type)]]" as="library">
   <template is="dom-if" if="[[library.occupancy]]">
@@ -112,6 +123,10 @@ class UvalibCounter extends PolymerElement {
   static get properties() {
     return {
       libraries: Array,
+      _geoworking: {
+        type: Boolean,
+        value: false
+      },
       _occupancy: {
         type: String,
         value: "occupancy"
@@ -130,8 +145,7 @@ class UvalibCounter extends PolymerElement {
 //        observer: "_setMode"
       },
       selectedLocation: {
-        type: String,
-        value: "centralGarageCovidTesting"
+        type: String
       },
       type: {
         type: String,
@@ -145,6 +159,59 @@ class UvalibCounter extends PolymerElement {
       }
     };
   }
+  _initSelectedLocation(){
+    this.selectedLocation = "centralGarageCovidTesting";
+  }
+  _locationAvailable(){
+    return navigator.geolocation
+  }
+  _locateMe(){
+    this._geoworking = true;
+    navigator.geolocation.getCurrentPosition(function(p){
+      this.location = {
+        lat: p.coords.latitude,
+        lng: p.coords.longitude
+      }  
+      // calc distances to locations and select the closest one
+      var shorty = this.selectedLocation;
+      var distance = 1000;
+      this._filter(this.libraries,this.type).forEach(function(p){
+        const d = this._distance(this.location.lat, this.location.lng, p.geo.latitude, p.geo.longitude);
+        if (distance >  d) {
+          distance = d;
+          shorty = p.key;
+        }
+      }.bind(this));
+      this.selectedLocation = shorty;
+      this._geoworking = false;
+    }.bind(this),
+    function(err){
+      this._geoworking = false
+    }.bind(this));
+  }
+
+  _distance(lat1, lon1, lat2, lon2, unit) {
+    if ((lat1 == lat2) && (lon1 == lon2)) {
+      return 0;
+    }
+    else {
+      var radlat1 = Math.PI * lat1/180;
+      var radlat2 = Math.PI * lat2/180;
+      var theta = lon1-lon2;
+      var radtheta = Math.PI * theta/180;
+      var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+      if (dist > 1) {
+        dist = 1;
+      }
+      dist = Math.acos(dist);
+      dist = dist * 180/Math.PI;
+      dist = dist * 60 * 1.1515;
+      if (unit=="K") { dist = dist * 1.609344 }
+      if (unit=="N") { dist = dist * 0.8684 }
+      return dist;
+    }
+  }
+
   ready() {
     
     super.ready();
